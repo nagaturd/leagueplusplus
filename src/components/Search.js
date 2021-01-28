a4a33437ab86a4fee53f6a377e7f17ffcfc25c82
kibumah <thebriankwon@gmail.com>
2021-01-27 22:09:24 -0500

    Removed unneeded complexities

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,44 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Profile from "./Profile.js";
 import { apiKey } from "../config.json";
 
 const Search = () => {
   const [summoner, setSummoner] = useState([]);
   const [search, setSearch] = useState("");
-  const [summonerName, setSummonerName] = useState("");
-
-  useEffect(() => {
-    getSummoner();
-  }, [summonerName]);
 
   // Updates when stuff is typed in search box
   const updateSearch = (e) => {
     setSearch(e.target.value);
   };
 
-  // Once search is submitted, set summoner name to be string from search
-  const getSearch = (e) => {
-    e.preventDefault();
-    setSummonerName(search);
-  };
-
   // Look up summoner using Riot API
-  const getSummoner = async () => {
-    console.log("Using this name:", summonerName);
-    let summoner = encodeURI(summonerName);
+  const getSummoner = async (e) => {
+    e.preventDefault();
+    let summoner = encodeURI(search);
     const url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner}?api_key=${apiKey}`;
 
     // Handles API call
     const response = await fetch(url);
+    console.log(response);
     const data = await response.json();
+    console.log(data);
 
-    console.log("Received from API:", data);
     setSummoner(data);
   };
 
   return (
     <div>
-      <form className="search-form" onSubmit={getSearch}>
+      <form className="search-form" onSubmit={getSummoner}>
         <input
           className="search-bar"
           type="text"
